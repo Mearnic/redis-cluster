0263677e5ea45232038f242f3b4e1848263d3a2e
mearnic <mearnic@icloud.com>
2022-02-06 02:35:20 +0800

    优化ip显示

--- show_ip.sh
+++ show_ip.sh
@@ -1,1 +1,1 @@
-for port in `seq 7000 7002`;do echo -n "$(docker inspect --format '{{ (index .NetworkSettings.Networks "redis-net").IPAddress }}' "redis-${port}")":${port} ' '; done;
+for port in `seq 7000 7002`;do echo -n "$(docker inspect --format '{{ (index .NetworkSettings.Networks "redis-net").IPAddress }}' "r${port}")":${port} ' '; done;
